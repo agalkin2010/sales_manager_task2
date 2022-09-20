46ba42ed6cf02697d57b84700c70dfe657fb3a53
Alexander Galkin <galkin.sania2010@yandex.ru>
2022-09-20 12:16:14 +0200

    Modified int to long

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] salesArray = {5, 7, 15, 3};
+        long[] salesArray = {5L, 7L, 15L, 3L};
 
         SalesManager salesManager = new SalesManager(salesArray);
 
